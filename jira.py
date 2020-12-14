d40baceeb24f09d1da9b34fb676e4faea04afa27
Stonesth <pierre.thonon@gmail.com>
2020-12-14 09:42:15 +0100

    Change the way to search/create properties file

--- jira.py
+++ jira.py
@@ -8,8 +8,9 @@ from datetime import datetime
 from selenium.webdriver.common.keys import Keys
 
 # -4 for the name of this project Jira
-save_path = dirname(__file__)[ : -4]
-propertiesFolder_path = save_path + "Properties"
+#save_path = dirname(__file__)[ : -4]
+save_path = os.path.dirname(os.path.abspath("__file__"))[ : -4]
+propertiesFolder_path = save_path + "\\"+ "Properties"
 
 # Example of used
 # user_text = tools.readProperty(propertiesFolder_path, 'Jira', 'user_text=')
