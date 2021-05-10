cc629c4eb1e8d88df6020ce71f9e0e2b33e6c411
Stonesth <pierre.thonon@gmail.com>
2021-05-10 13:36:16 +0200

    Error - don t find the tinymce id

--- jira.py
+++ jira.py
@@ -306,6 +306,7 @@ def placeTheTextIntoComment(incidentNumber, incidentTitle) :
     # comment = tools.driver.find_element_by_id("comment")
 
     WebDriverWait(tools.driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//*[@id="mce_0_ifr"]')))
+    WebDriverWait(tools.driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//*[@id="tinymce"]/p')))
     comment = WebDriverWait(tools.driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="tinymce"]/p')))
     comment.send_keys(incidentNumber + " - " + incidentTitle + "\n" + "https://nnbe.topdesk.net/tas/secure/incident?action=lookup&lookup=naam&lookupValue=" + incidentNumber + "\n")
     # Need to exit the iFrame
