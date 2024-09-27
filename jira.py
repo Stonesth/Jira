cec7284fcee3a12ce71e90ed93424646eb1d417a
Stonesth <pierre.thonon@gmail.com>
2024-09-27 15:40:52 +0200

    change the id for description again

--- jira.py
+++ jira.py
@@ -317,7 +317,7 @@ def createJira(jiraTitle, description_text, incidentNumber, teamName, reporterNa
     # description = tools.driver.find_element(By.ID, "mce_7_ifr") # since the 2024-02-19
     # description = tools.driver.find_element(By.ID, "description") # since the 2024-03-18
     # description = tools.driver.find_element(By.ID, "mce_7_ifr") # since the 2024-03-21
-    description = tools.driver.find_element(By.ID, "description-val") # since the 2024-09-27
+    description = tools.driver.find_element(By.ID, "tinymce") # since the 2024-09-27
     description.click()
     # wait = WebDriverWait(tools.driver, 10)
     # description = wait.until(EC.invisibility_of_element_located((By.ID, 'description-wiki-edit')))
